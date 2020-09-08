65b09e6856306dbba54f0218cb2fdf81393d1910
unknown <jmsanjuan0@misena.edu.co>
2020-09-08 01:04:09 -0500

    Readme Update

---
+++ larapp/database/migrations/2020_09_07_021400_create_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('fullname');
+            $table->string('photo')->default("imgs/no-photo.png");
+            $table->text('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
---
+++ larapp/database/migrations/2020_09_08_041627_create_games_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('fullname');
+            $table->string('photo')->default("imgs/no-photo.png");
+            $table->text('description');
+            $table->bigInteger('user_id');
+            $table->bigInteger('category_id');
+            $table->tinyInteger('slider');
+            $table->double('price', 8, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
