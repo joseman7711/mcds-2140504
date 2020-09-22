f4822f0736b2fd5e1f98a253d6e3b20dee57c55e
Joseman <jmsanjuan0@misena.edu.co>
2020-09-22 11:21:30 -0500

    homew

--- larapp/routes/web.php
+++ larapp/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Carbon\Carbon;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('challenge', function () {
+    foreach (App\User::all()->take(10) as $user) {
+        $years = Carbon::createFromDate($user->birthdate)->diff()->format('%y years old %m %d');
+        $since = Carbon::parse($user->created_at);
+        $rs[] = $user->name." - ".$years." - created ".$since->diffForHumans();
+        }
+        dd($rs);
+    });
